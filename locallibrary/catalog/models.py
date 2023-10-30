477472a8e5449aa628195deab27f6f0bb88d11d8
gr421nidan <nikushkina.dasha@yandex.ru>
2023-10-31 01:56:21 +0700

    add file

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+from datetime import date
 
 class Genre(models.Model):
     """Model representing a book genre."""
@@ -50,6 +54,14 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
+
+
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -66,8 +78,10 @@ class BookInstance(models.Model):
         help_text='Book availability',
     )
 
+
     class Meta:
         ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self):
         """String for representing the Model object."""
