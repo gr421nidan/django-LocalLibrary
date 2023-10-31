8eb879d47c1f16f69ef8f4e033e8b297d259047c
gr421nidan <nikushkina.dasha@yandex.ru>
2023-10-31 12:42:09 +0700

    Обновление html файла из-за допущенной ошибки

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -89,13 +89,19 @@ class BookInstance(models.Model):
 
 
 class Author(models.Model):
+    """Model representing an author."""
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = models.DateField('Died', null=True, blank=True)
+    date_of_death = models.DateField('died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
 
     def get_absolute_url(self):
+        """Returns the url to access a particular author instance."""
         return reverse('author-detail', args=[str(self.id)])
 
     def __str__(self):
-        return '%s, %s' % (self.last_name, self.first_name)
+        """String for representing the Model object."""
+        return '{0}, {1}'.format(self.last_name, self.first_name)
